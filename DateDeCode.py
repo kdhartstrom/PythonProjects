59e12db1f1cd6b9640eca1aeeeca0024330794ce
kdhartstrom <kdhartstrom@mono.ca.gov>
2017-02-23 21:53:44 -0800

    Made more efficient
    used an operator for add / subtract, then I don't have duplicate as much code

--- DateDeCode.py
+++ DateDeCode.py
@@ -1,47 +1,45 @@
 # Caleb's Decoding Program, Feb 14, 2016
+import operator
 
 #Date Pattern that we use to decode / encode, should be all numeric, any length
-pattern = '2182016'
+pattern = '09192016'
 
-#either 'add' or 'subtract'
-method ='add'
+
+#either operator.add or operator.sub if you are going up or down in the alphabet
+updown = operator.sub
+
 
 #this can ether be clear text, to encode, or coded text to decode
 string_in = """
-Dear Caleb,
-I just wanted you to know that I'm pleased with how you are learning to be a responsible man, 
-and I appreciate it when you show kindness to your brothers and sisters.
-Love,
-Your Dad
+Dnba Eamkb,
+Spbjub os mprpg xklu. J qqpf eod fwlozkd xva xitot jom K
+cbt't fbrv tp yen zxw ahgiw gxt Cixibuvcs. Uu hnmy mefv
+ods wcmfy sndagt, J'rl lbun ypa Fjmlqn. Eu yxv tpox chju
+hqus mrjomra xgnct oqr Dnrrtcoat?
+
+
+   Mhxtc (Reukr) 
 """
 
 
 #the rest of this stuff should just work
+upper = range(65,90)
+lower = range(97,122)
+if updown is operator.sub : opposite = operator.add
+else : opposite = operator.sub
 string_out = ''
 pattern_position = 0
 for c in string_in:
-	#change our character to an ascii code
-	n = ord(c)
-	# test to see if it's a letter
-	if 67 <= n <= 90 or 97 <= n <= 122:
-		if method == 'subtract' :
-			n_out = n - int(pattern[pattern_position])
-			#check if we still have an alphabet character
-			if 67 <= n_out <= 90 or 97 <= n_out <= 122 :
-				c_out = chr(n_out)
-			else:
-				#we have to start the alphabet over because we are out of range
-				c_out = chr(n_out + 26)
-				
-		if method == 'add' :
-			n_out = n + int(pattern[pattern_position])
-			#check if we still have an alphabet character
-			if 67 <= n_out <= 90 or 97 <= n_out <= 122 :
-				c_out = chr(n_out)
-			else:
-				#we have to start the alphabet over because we are out of range
-				c_out = chr(n_out - 26)
-
+	if c.isalpha():
+	 	if c.isupper() :
+			n_out = updown(ord(c), int(pattern[pattern_position]))
+			if n_out in upper : c_out = chr(n_out)
+			else: c_out = chr(opposite(n_out, 26))
+		if c.islower():
+			n_out = updown(ord(c), int(pattern[pattern_position]))
+			if n_out in lower : c_out = chr(n_out)
+			else: c_out = chr(opposite(n_out, 26))
+	
 		#append our character to the string out
 		string_out += c_out
 		
